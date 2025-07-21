32924ebd7d23762b7298504680bb5d11f3101286
VoxlD <helloneighbor159@gmail.com>
2025-07-22 06:24:15 +0700

    fix misspelled folder name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export * from "./types";
 
 // Classes
-export { Oxarion as default, Oxarion } from "./adatper";
+export { Oxarion as default, Oxarion } from "./adapter";
 export { RoutesWrapper } from "./route/wrapper";
 export { ParsedFormData } from "./form_data";
 export * as Middleware from "./middleware/built_in";
